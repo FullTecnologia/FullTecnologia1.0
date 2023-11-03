2fcd48cbca97803cc2694885fde7d669bdbc1f4b
juliamellolopes <juliamellolopesgoncalves@gmail.com>
2023-11-03 14:44:25 -0300

    popup cadastrar atividade

--- frontend/src/pages/RecursosHumanos/RecursosHumanos.js
+++ frontend/src/pages/RecursosHumanos/RecursosHumanos.js
@@ -4,6 +4,7 @@ import { useLocation } from 'react-router-dom';
 import Table from '../../components/Table/Table';
 import styles from './RecursosHumanos.module.css';
 import NavBar from '../../components/NavBar/NavBar';
+import { validateForm } from '../../hooks/validation';
 import '../../components/Popup/style.css';
 
 function RecursosHumanos() {
@@ -17,6 +18,16 @@ function RecursosHumanos() {
         // Adicione mais dados conforme necessário
     ];
 
+    const [atividade, setAtividade] = useState({
+        responsavel: '',
+        descricao: '',
+        dataFim: '',
+        status: 'iniciado',
+        tipo: 0,
+    });
+
+    const [validationErrors, setValidationErrors] = useState({});
+
     function showModal() {
         var element = document.getElementById("modal");
         element.classList.add("show-modal");
@@ -25,6 +36,133 @@ function RecursosHumanos() {
     function closeModal() {
         var element = document.getElementById("modal");
         element.classList.remove("show-modal");
+    
+        // Redefina o estado 'atividade' para valores iniciais
+        setAtividade({
+            responsavel: '',
+            descricao: '',
+            dataFim: '',
+            status: 'iniciado',
+            tipo: 0,
+        });
+    
+        // Limpe quaisquer erros de validação
+        setValidationErrors({});
+    }
+
+    function handleInputChange(event) {
+        const { name, value } = event.target;
+        setAtividade({
+            ...atividade,
+            [name]: value,
+        })
+    }   
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        const errors = validateForm(atividade);
+    
+        if (Object.keys(errors).length === 0) {
+            // Dados do formulário são válidos
+            fetch("/cadastrarAtividade", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(atividade), // Envia os dados como JSON
+            })
+                .then((response) => {
+                    if (response.ok) {
+                        // A solicitação foi bem-sucedida
+                        return response.json();
+                    } else {
+                        // A solicitação falhou
+                        throw new Error("Erro na solicitação");
+                    }
+                })
+                .then((data) => {
+                    // Os dados de resposta do servidor, se houver
+                    console.log(data);
+                    closeModal();
+                })
+                .catch((error) => {
+                    // Lidar com erros de solicitação
+                    console.error(error);
+                });
+        } else {
+            setValidationErrors(errors);
+        }
+    }
+
+    function renderForm() {
+        return (
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <label htmlFor="responsavel">Responsável:</label>
+                    <input
+                        type="text"
+                        id="responsavel"
+                        name="responsavel"
+                        value={atividade.responsavel}
+                        onChange={handleInputChange}
+                        className="form-input"
+                    />
+                    <span className="error">{validationErrors.responsavel}</span>
+                </div>
+                <div>
+                    <label htmlFor="descricao">Descrição:</label>
+                    <input
+                        type="text"
+                        id="descricao"
+                        name="descricao"
+                        value={atividade.descricao}
+                        onChange={handleInputChange}
+                    />
+                    <span className="error">{validationErrors.descricao}</span>
+                </div>
+                <div>
+                    <label htmlFor="dataFim">Data de Fim:</label>
+                    <input
+                        type="date"
+                        id="dataFim"
+                        name="dataFim"
+                        value={atividade.dataFim}
+                        onChange={handleInputChange}
+                    />
+                    <span className="error">{validationErrors.dataFim}</span>
+                </div>
+                <div>
+                    <label htmlFor="status">Status:</label>
+                    <select
+                        id="status"
+                        name="status"
+                        value={atividade.status}
+                        onChange={handleInputChange}
+                    >
+                        <option value="iniciado">Iniciado</option>
+                        <option value="concluido">Concluído</option>
+                        <option value="finalizado">Finalizado</option>
+                    </select>
+                    <span className="error">{validationErrors.status}</span>
+                </div>
+                <div>
+                    <label htmlFor="tipo">Tipo:</label>
+                    <select
+                        id="tipo"
+                        name="tipo"
+                        value={atividade.tipo}
+                        onChange={handleInputChange}
+                    >
+                        <option value="0">Não Prioritário</option>
+                        <option value="1">Prioritário</option>
+                    </select>
+                    <span className="error">{validationErrors.tipo}</span>
+                </div>
+                <div>
+                    <button type="submit" className="submit-button">Cadastrar</button>
+                </div>
+            </form>
+        );
     }
     
     return (
@@ -42,6 +180,7 @@ function RecursosHumanos() {
                                 &times;
                             </span>
                             <h1>Cadastrar Atividade</h1>
+                            {renderForm()}
                         </div>
                     </div>
                 </div>
