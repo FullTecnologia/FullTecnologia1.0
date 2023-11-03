77d742f1edbe942208f2ddf6242bd61a7aa292ea
EduardoGrillo <grillo.dudu@yahoo.com>
2023-11-03 09:40:21 -0300

    tabela atualizada

--- frontend/src/components/Table/Table.js
+++ frontend/src/components/Table/Table.js
@@ -1,11 +1,36 @@
-import React from 'react'
+import React from 'react';
+import styles from './Table.module.css'; // Importe as classes CSS do arquivo separado
 
-import styles from "./Table.module.css";
-
-const Table = () => {
+const Table = ({ data }) => {
     return (
-        <div>Table</div>
-    )
+        <div>
+            <h3 className={styles.title}>Tabela de Atividades Programadas</h3>
+            <table className={styles.tableContainer}>
+                <thead>
+                    <tr>
+                        <th className={styles.tableHeader}>Título</th>
+                        <th className={styles.tableHeader}>Responsável</th>
+                        <th className={styles.tableHeader}>Descrição</th>
+                        <th className={styles.tableHeader}>Data-Fim</th>
+                        <th className={styles.tableHeader}>Status</th>
+                        <th className={styles.tableHeader}>Tipo</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {data.map(item => (
+                        <tr key={item.id}>
+                            <td className={styles.tableCell}>{item.titulo}</td>
+                            <td className={styles.tableCell}>{item.responsavel}</td>
+                            <td className={styles.tableCell}>{item.descricao}</td>
+                            <td className={styles.tableCell}>{item.dataFim}</td>
+                            <td className={styles.tableCell}>{item.status}</td>
+                            <td className={styles.tableCell}>{item.tipo}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    );
 }
 
-export default Table
+export default Table;
--- frontend/src/pages/RecursosHumanos/RecursosHumanos.js
+++ frontend/src/pages/RecursosHumanos/RecursosHumanos.js
@@ -1,13 +1,21 @@
 import React, { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
-import NavBar from '../../components/NavBar/NavBar'; // Importe a NavBar
-import styles from './RecursosHumanos.module.css'; // Importe o arquivo de módulo CSS
+import NavBar from '../../components/NavBar/NavBar';
+import Table from '../../components/Table/Table'; // Importe o componente Table
+import styles from './RecursosHumanos.module.css';
 
 function RecursosHumanos() {
     const location = useLocation();
     const searchParams = new URLSearchParams(location.search);
     const nomeDoUsuario = searchParams.get('nome');
     const emailDoUsuario = searchParams.get('email');
+
+    // Dados fictícios (substitua por dados reais do banco de dados)
+    const dadosDaTabela = [
+        { id: 1, nome: 'Atividade 1', data: '2023-11-01' },
+        { id: 2, nome: 'Atividade 2', data: '2023-11-02' },
+        // Adicione mais dados conforme necessário
+    ];
 
     useEffect(() => {
         console.log('Nome do Usuário:', nomeDoUsuario);
@@ -16,11 +24,14 @@ function RecursosHumanos() {
 
     return (
         <div>
-            <NavBar /> {/* Renderize a NavBar sem passar informações de usuário */}
+            <NavBar />
+
             <div className={styles.bodyRecursos}>
                 <h2>Recursos Humanos</h2>
                 <p>Essa página ficará encarregada do RH</p>
             </div>
+
+            <Table data={dadosDaTabela} /> {/* Passe os dados para o componente Table */}
         </div>
     );
 }
