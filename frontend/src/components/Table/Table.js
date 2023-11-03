92f4d9ca5c39ad420e518fe795d07da9d726fcaa
EduardoGrillo <grillo.dudu@yahoo.com>
2023-11-03 14:03:47 -0300

    att tabela

--- frontend/src/components/Table/Table.js
+++ frontend/src/components/Table/Table.js
@@ -1,36 +1,55 @@
-import React from 'react';
-import styles from './Table.module.css'; // Importe as classes CSS do arquivo separado
+import React, { useState, useEffect } from 'react';
+import styles from './Table.module.css'; // Importe seu arquivo CSS
+import axios from 'axios';
 
-const Table = ({ data }) => {
+const Table = () => {
+    const [atividades, setAtividades] = useState([]);
+
+    useEffect(() => {
+        // Função para buscar os dados do banco de dados
+        const fetchDataFromDatabase = async () => {
+            try {
+                const response = await axios.get('http://localhost:3003/api/sua-rota-aqui'); // Substitua pela rota correta
+
+                if (response.status === 200) {
+                    setAtividades(response.data);
+                } else {
+                    console.error('Erro ao buscar dados do banco de dados.');
+                }
+            } catch (error) {
+                console.error('Erro ao buscar dados do banco de dados:', error);
+            }
+        };
+
+        // Chama a função para buscar os dados ao carregar o componente
+        fetchDataFromDatabase();
+    }, []);
+
     return (
         <div>
-            <h3 className={styles.title}>Tabela de Atividades Programadas</h3>
-            <table className={styles.tableContainer}>
-                <thead>
+            <h1 className={styles.textTable}>Tabela de Atividades</h1>
+            <table className={styles.centeredTable}> {/* Aplicando as classes CSS */}
+                <thead className={styles.table}>
                     <tr>
-                        <th className={styles.tableHeader}>Título</th>
-                        <th className={styles.tableHeader}>Responsável</th>
-                        <th className={styles.tableHeader}>Descrição</th>
-                        <th className={styles.tableHeader}>Data-Fim</th>
-                        <th className={styles.tableHeader}>Status</th>
-                        <th className={styles.tableHeader}>Tipo</th>
+                        <th>ID</th>
+                        <th>Nome da Atividade</th>
+                        <th>Data</th>
+                        {/* Adicione mais colunas conforme necessário */}
                     </tr>
                 </thead>
                 <tbody>
-                    {data.map(item => (
-                        <tr key={item.id}>
-                            <td className={styles.tableCell}>{item.titulo}</td>
-                            <td className={styles.tableCell}>{item.responsavel}</td>
-                            <td className={styles.tableCell}>{item.descricao}</td>
-                            <td className={styles.tableCell}>{item.dataFim}</td>
-                            <td className={styles.tableCell}>{item.status}</td>
-                            <td className={styles.tableCell}>{item.tipo}</td>
+                    {atividades.map((atividade) => (
+                        <tr key={atividade.id}>
+                            <td>{atividade.id}</td>
+                            <td>{atividade.nome_atividade}</td>
+                            <td>{atividade.data_atividade}</td>
+                            {/* Adicione mais colunas conforme necessário */}
                         </tr>
                     ))}
                 </tbody>
             </table>
-        </div>
+        </div >
     );
-}
+};
 
 export default Table;
--- frontend/src/pages/RecursosHumanos/RecursosHumanos.js
+++ frontend/src/pages/RecursosHumanos/RecursosHumanos.js
@@ -1,47 +1,19 @@
-import React, { useEffect, useState } from 'react';
-import { useLocation } from 'react-router-dom';
-import Table from '../../components/Table/Table'; // Importe o componente Table
+// RecursosHumanos.js
+import React from 'react';
 import styles from './RecursosHumanos.module.css';
-import NavBar from '../../components/NavBar/NavBar'; // Importe a NavBar
-import Popup from '../../components/Popup/Popup';
+import NavBar from '../../components/NavBar/NavBar';
+import Table from '../../components/Table/Table';
 
 function RecursosHumanos() {
-    const location = useLocation();
-    const searchParams = new URLSearchParams(location.search);
-
-    // Dados fictícios (substitua por dados reais do banco de dados)
-    const dadosDaTabela = [
-        { id: 1, nome: 'Atividade 1', data: '2023-11-01' },
-        { id: 2, nome: 'Atividade 2', data: '2023-11-02' },
-        // Adicione mais dados conforme necessário
-    ];
-
-    const [mostrarPopup, setMostrarPopup] = useState(false); // Adicione este estado
-
-    const abrirPopup = () => {
-        setMostrarPopup(true);
-    };
-
-    const fecharPopup = () => {
-        setMostrarPopup(false);
-    };
-
     return (
         <div>
             <NavBar />
-
             <div className={styles.bodyRecursos}>
                 <h2>Recursos Humanos</h2>
                 <p>Essa página ficará encarregada do RH</p>
-                <div>
-                    <button onClick={abrirPopup}>
-                        Cadastrar Atividade
-                    </button>
-                    {<Popup mostrar={abrirPopup} />}
-                </div>
             </div>
 
-            <Table data={dadosDaTabela} /> {/* Passe os dados para o componente Table */}
+            <Table />
         </div>
     );
 }
