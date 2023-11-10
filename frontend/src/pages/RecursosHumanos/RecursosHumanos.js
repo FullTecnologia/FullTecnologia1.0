7a18781f7f91de594403c330e114c502c5da3e68
juliamellolopes <juliamellolopesgoncalves@gmail.com>
2023-11-10 13:06:22 -0300

    renderColaborador

--- frontend/src/pages/RecursosHumanos/RecursosHumanos.js
+++ frontend/src/pages/RecursosHumanos/RecursosHumanos.js
@@ -1,250 +1,224 @@
-import React, { useState } from 'react';
-import { Table, Colab } from '../../components/Table/Table';
-import NavBar from '../../components/NavBar/NavBar';
-import { validateForm } from '../../hooks/validation';
-import '../../components/Popup/style.css';
-import styles from './RecursosHumanos.module.css';
-import { RegistroColaborador, initialState } from './renderColaborador';
-import ReactDOM from 'react-dom';
+import React, { useState } from "react";
+import { Table, Colab } from "../../components/Table/Table";
+import NavBar from "../../components/NavBar/NavBar";
+import { validateForm } from "../../hooks/validation";
+import "../../components/Popup/style.css";
+import styles from "./RecursosHumanos.module.css";
+import { RegistroColaborador, initialState } from "./renderColaborador";
+import ReactDOM from "react-dom";
 
-function closeModal(modalId,  setAtividade, setColaborador, setValidationErrors) {
-        var element = document.getElementById(modalId);
-        element.classList.remove("show-modal");
+function closeModal(
+  modalId,
+  setAtividade,
+  setColaborador,
+  setValidationErrors
+) {
+  var element = document.getElementById(modalId);
+  element.classList.remove("show-modal");
 
-        setAtividade({
-            responsavel: '',
-            descricao: '',
-            dataFim: '',
-            status: 'iniciado',
-            tipo: 0,
-        });
+  setAtividade({
+    responsavel: "",
+    descricao: "",
+    dataFim: "",
+    status: "iniciado",
+    tipo: 0,
+  });
 
-        setColaborador({
-            usuario: {
-                nome: '',
-                email: '',
-                senha: '',
-                nivel: 0,
-            },
-            ficha: {
-                data_nascimento: '',
-                naturalidade: '',
-                nome_mae: '',
-                nome_pai: '',
-                cpf: '',
-                carteira_identidade: '',
-                expedidor_identidade: '',
-                data_emissao_identidade: '',
-                titulo_eleitor_numero: '',
-                titulo_eleitor_zona: '',
-                titulo_eleitor_secao: '',
-                ctps_numero: '',
-                ctps_serie: '',
-                ctps_uf: '',
-                ctps_data_emissao: '',
-                pis_numero: '',
-                pis_data_cadastro: '',
-                carteira_habilitacao_numero: '',
-                carteira_habilitacao_categoria: '',
-                estado_civil: '',
-                escolaridade: '',
-                raca_cor: '',
-                certificado_reservista_numero: '',
-                certificado_reservista_categoria: '',
-                nome_companheiro: '',
-                endereco: '',
-                endereco_numero: '',
-                endereco_cep: '',
-                endereco_complemento: '',
-                endereco_cidade: '',
-                endereco_bairro: '',
-                endereco_uf: '',
-                cargo: '',
-                cbo: '',
-                data_admissao: '',
-                salario: '',
-                contrato_experiencia: '',
-                horario_trabalho: '',
-                intervalo: '',
-                descanso: '',
-                horario_sabado: '',
-                vale_transporte: false,
-                informacoes_complementares: '',
-            },
-            habilidades: {
-                habilidade: '',
-                especialidade: '',
-            },
-        })
+  setColaborador(initialState);
 
-        // Limpe quaisquer erros de validação
-        setValidationErrors({});
+  // Limpe quaisquer erros de validação
+  setValidationErrors({});
 }
 
 function RecursosHumanos() {
-    const [atividade, setAtividade] = useState({
-        responsavel: '',
-        descricao: '',
-        dataFim: '',
-        status: 'iniciado',
-        tipo: 0,
+  const [atividade, setAtividade] = useState({
+    responsavel: "",
+    descricao: "",
+    dataFim: "",
+    status: "iniciado",
+    tipo: 0,
+  });
+
+  const [colaborador, setColaborador] = useState(initialState);
+  const [validationErrors, setValidationErrors] = useState({});
+
+  function showModal(modalType) {
+    var element = document.getElementById(`modal${modalType}`);
+    if (element) {
+      element.classList.add("show-modal");
+    }
+  }
+
+  function handleInputChange(event) {
+    const { name, value } = event.target;
+    setAtividade({
+      ...atividade,
+      [name]: value,
     });
+  }
 
-    const [colaborador, setColaborador] = useState(initialState);
-    const [validationErrors, setValidationErrors] = useState({});
-    
-   
-    function showModal(modalType) {
-        var element = document.getElementById(`modal${modalType}`);
-        if (element) {
-            element.classList.add("show-modal");
-        }
-    }
+  function handleSubmit(event) {
+    event.preventDefault();
+    const errors = validateForm(atividade);
 
-    function handleInputChange(event) {
-        const { name, value } = event.target;
-        setAtividade({
-            ...atividade,
-            [name]: value,
+    if (Object.keys(errors).length === 0) {
+      // Dados do formulário de atividade são válidos
+      fetch("/cadastrarAtividade", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(atividade),
+      })
+        .then((response) => {
+          if (response.ok) {
+            return response.json();
+          } else {
+            throw new Error("Erro na solicitação");
+          }
+        })
+        .then((data) => {
+          console.log(data);
+          closeModal();
+        })
+        .catch((error) => {
+          console.error(error);
         });
+    } else {
+      setValidationErrors(errors);
     }
+  }
 
-    function handleSubmit(event) {
-        event.preventDefault();
-        const errors = validateForm(atividade);
-
-        if (Object.keys(errors).length === 0) {
-            // Dados do formulário de atividade são válidos
-            fetch("/cadastrarAtividade", {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify(atividade),
-            })
-                .then((response) => {
-                    if (response.ok) {
-                        return response.json();
-                    } else {
-                        throw new Error("Erro na solicitação");
-                    }
-                })
-                .then((data) => {
-                    console.log(data);
-                    closeModal();
-                })
-                .catch((error) => {
-                    console.error(error);
-                });
-        } else {
-            setValidationErrors(errors);
-        }
-    }
-
-    function renderForm() {
-        return (
-            <form onSubmit={handleSubmit}>
-                <div>
-                    <label htmlFor="responsavel">Responsável:</label>
-                    <input
-                        type="text"
-                        id="responsavel"
-                        name="responsavel"
-                        value={atividade.responsavel}
-                        onChange={handleInputChange}
-                        className="form-input"
-                    />
-                    <span className="error">{validationErrors.responsavel}</span>
-                </div>
-                <div>
-                    <label htmlFor="descricao">Descrição:</label>
-                    <input
-                        type="text"
-                        id="descricao"
-                        name="descricao"
-                        value={atividade.descricao}
-                        onChange={handleInputChange}
-                    />
-                    <span className="error">{validationErrors.descricao}</span>
-                </div>
-                <div>
-                    <label htmlFor="dataFim">Data de Fim:</label>
-                    <input
-                        type="date"
-                        id="dataFim"
-                        name="dataFim"
-                        value={atividade.dataFim}
-                        onChange={handleInputChange}
-                    />
-                    <span className="error">{validationErrors.dataFim}</span>
-                </div>
-                <div>
-                    <label htmlFor="status">Status:</label>
-                    <select
-                        id="status"
-                        name="status"
-                        value={atividade.status}
-                        onChange={handleInputChange}
-                    >
-                        <option value="iniciado">Iniciado</option>
-                        <option value="concluido">Concluído</option>
-                        <option value="finalizado">Finalizado</option>
-                    </select>
-                    <span className="error">{validationErrors.status}</span>
-                </div>
-                <div>
-                    <label htmlFor="tipo">Tipo:</label>
-                    <select
-                        id="tipo"
-                        name="tipo"
-                        value={atividade.tipo}
-                        onChange={handleInputChange}
-                    >
-                        <option value="0">Não Prioritário</option>
-                        <option value="1">Prioritário</option>
-                    </select>
-                    <span className="error">{validationErrors.tipo}</span>
-                </div>
-                <div>
-                    <button type="submit" className="submit-button">Cadastrar</button>
-                </div>
-            </form>
-        );
-    }
-
+  function renderForm() {
     return (
+      <form onSubmit={handleSubmit}>
         <div>
-            <NavBar />
-            <div className={styles.bodyRecursos}>
-                <div>
-                    <button className="trigger" onClick={() => showModal('Atividade')}>Cadastrar Atividade</button>
-                    <div className="modal" id="modalAtividade">
-                        <div className="modal-content">
-                            <span className="close-button" onClick={() => closeModal('modalAtividade', setAtividade, setColaborador, setValidationErrors)}>
-                                &times;
-                            </span>
-                            <h1>Cadastrar Atividade</h1>
-                            {renderForm()}
-                        </div>
-                    </div>
-                    <button className="trigger" onClick={() => showModal('Colaborador')}>Cadastrar Colaborador</button>
-                    <div className="modal" id="modalColaborador">
-                        <div className="modal-content">
-                            <span className="close-button" onClick={() => closeModal('modalColaborador', setAtividade, setColaborador, setValidationErrors)}>
-                                &times;
-                            </span>
-                            <h1>Cadastrar Colaborador</h1>
-                            <RegistroColaborador/>
-                        </div>
-                    </div>
-                </div>
-                <div>
-                    <Table />
-                    <Colab />
-                </div>
-            </div>
+          <label htmlFor="responsavel">Responsável:</label>
+          <input
+            type="text"
+            id="responsavel"
+            name="responsavel"
+            value={atividade.responsavel}
+            onChange={handleInputChange}
+            className="form-input"
+          />
+          <span className="error">{validationErrors.responsavel}</span>
         </div>
+        <div>
+          <label htmlFor="descricao">Descrição:</label>
+          <input
+            type="text"
+            id="descricao"
+            name="descricao"
+            value={atividade.descricao}
+            onChange={handleInputChange}
+          />
+          <span className="error">{validationErrors.descricao}</span>
+        </div>
+        <div>
+          <label htmlFor="dataFim">Data de Fim:</label>
+          <input
+            type="date"
+            id="dataFim"
+            name="dataFim"
+            value={atividade.dataFim}
+            onChange={handleInputChange}
+          />
+          <span className="error">{validationErrors.dataFim}</span>
+        </div>
+        <div>
+          <label htmlFor="status">Status:</label>
+          <select
+            id="status"
+            name="status"
+            value={atividade.status}
+            onChange={handleInputChange}
+          >
+            <option value="iniciado">Iniciado</option>
+            <option value="concluido">Concluído</option>
+            <option value="finalizado">Finalizado</option>
+          </select>
+          <span className="error">{validationErrors.status}</span>
+        </div>
+        <div>
+          <label htmlFor="tipo">Tipo:</label>
+          <select
+            id="tipo"
+            name="tipo"
+            value={atividade.tipo}
+            onChange={handleInputChange}
+          >
+            <option value="0">Não Prioritário</option>
+            <option value="1">Prioritário</option>
+          </select>
+          <span className="error">{validationErrors.tipo}</span>
+        </div>
+        <div>
+          <button type="submit" className="submit-button">
+            Cadastrar
+          </button>
+        </div>
+      </form>
     );
+  }
+
+  return (
+    <div>
+      <NavBar />
+      <div className={styles.bodyRecursos}>
+        <div>
+          <button className="trigger" onClick={() => showModal("Atividade")}>
+            Cadastrar Atividade
+          </button>
+          <div className="modal" id="modalAtividade">
+            <div className="modal-content">
+              <span
+                className="close-button"
+                onClick={() =>
+                  closeModal(
+                    "modalAtividade",
+                    setAtividade,
+                    setColaborador,
+                    setValidationErrors
+                  )
+                }
+              >
+                &times;
+              </span>
+              <h1>Cadastrar Atividade</h1>
+              {renderForm()}
+            </div>
+          </div>
+          <button className="trigger" onClick={() => showModal("Colaborador")}>
+            Cadastrar Colaborador
+          </button>
+          <div className="modal" id="modalColaborador">
+            <div className="modal-content">
+              <span
+                className="close-button"
+                onClick={() =>
+                  closeModal(
+                    "modalColaborador",
+                    setAtividade,
+                    setColaborador,
+                    setValidationErrors
+                  )
+                }
+              >
+                &times;
+              </span>
+              <h1>Cadastrar Colaborador</h1>
+              <RegistroColaborador />
+            </div>
+          </div>
+        </div>
+        <div>
+          <Table />
+          <Colab />
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export {RecursosHumanos, closeModal};
+export { RecursosHumanos, closeModal };
