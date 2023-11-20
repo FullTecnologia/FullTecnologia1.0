8675ad6670051211caf08cc7f1e0c0a3f9b0b65f
juliamellolopes <juliamellolopesgoncalves@gmail.com>
2023-11-20 17:26:19 -0300

    add extends

--- backend/src/config/configDataBase.js
+++ backend/src/config/configDataBase.js
@@ -1,31 +1,17 @@
-<<<<<<< HEAD
 import { Sequelize } from "sequelize";
 
-const sequelize = new Sequelize('homologfull', 'root', '123456', {
-    host: 'localhost',
-    dialect: 'mysql'
+const sequelize = new Sequelize("homologfull", "root", "123456", {
+  host: "localhost",
+  dialect: "mysql",
 });
 
-sequelize.authenticate().then(() => {
-    console.log('Conexão com o banco de dados MySql realizada com sucesso!');
-}).catch((error) => {
-    console.error('Erro ao conectar com banco de dados MySql: ', error);
-});
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log("Conexão com o banco de dados MySql realizada com sucesso!");
+  })
+  .catch((error) => {
+    console.error("Erro ao conectar com banco de dados MySql: ", error);
+  });
 
 export { sequelize };
-=======
-import { Sequelize } from "sequelize";
-
-const sequelize = new Sequelize('homologfull', 'root', '123456', {
-    host: 'localhost',
-    dialect: 'mysql'
-});
-
-sequelize.authenticate().then(() => {
-    console.log('Conexão com o banco de dados MySql realizada com sucesso!');
-}).catch((error) => {
-    console.error('Erro ao conectar com banco de dados MySql: ', error);
-});
-
-export { sequelize };
->>>>>>> master
