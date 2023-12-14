219927da392cd8f95bd00aebd8d672cb4ef47635
EduardoGrillo <grillo.dudu@yahoo.com>
2023-12-14 13:41:32 -0300

    remove

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,15 +4,7 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { AuthProvider } from '../src/contexts/AuthContext';
 
 import Login from "./pages/Login/Login";
-import Register from "./pages/Register/Register";
 import RecursosHumanos from "./pages/RecursosHumanos/RecursosHumanos";
-import Home from "./pages/Home/Home";
-import Profissional from "./pages/Profissional/Profissional";
-import Lider from "./pages/Lider/Lider";
-import Planejamento from "./pages/Planejamento/Planejamento";
-import Comercial from "./pages/Comercial/Comercial";
-import Financeiro from "./pages/Financeiro/Financeiro";
-import Diretoria from "./pages/Diretoria/Diretoria";
 import CadastrarColaborador from "./pages/RecursosHumanos/cadastrarColab";
 
 function App() {
@@ -21,19 +13,8 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-
-          {/* Adicione a rota a seguir para redirecionar para a página de registro */}
-          <Route path="/clique-aqui" element={<Navigate to="/register" />} />
 
           <Route path="/recursoshumanos" element={<RecursosHumanos />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/profissional" element={<Profissional />} />
-          <Route path="/lider" element={<Lider />} />
-          <Route path="/planejamento" element={<Planejamento />} />
-          <Route path="/comercial" element={<Comercial />} />
-          <Route path="/financeiro" element={<Financeiro />} />
-          <Route path="/diretoria" element={<Diretoria />} />
 
           <Route
             path="/cadastrarColaborador"
